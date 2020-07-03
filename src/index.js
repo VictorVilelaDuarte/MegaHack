3c89a60f9da4bf1755c7ba58fc23203d022bc049
Victor Vilela Duarte <victorvduarte@gmail.com>
2020-07-03 13:58:18 -0300

    Melhorias no background e na statusbar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import App from './App';
 
 export default function src() {
   return (
-    <>
-      <SafeAreaProvider>
-        <StatusBar
-          backgroundColor="#FDDC13"
-          barStyle="dark-content"
-          translucent
-        />
-        <App />
-      </SafeAreaProvider>
-    </>
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
   );
 }
